0fb5ac681fa7941c0d5989b9851aeca0c4d320c4
Erik Radovicevic <radovicevic.erik1@gmail.com>
2023-12-02 10:47:40 +0100

    New commit

--- domacenaloge/05/DN05_63230273.java
+++ domacenaloge/05/DN05_63230273.java
@@ -1,15 +1,9 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class DN05_63230273{
     public static void main(String[] args){
 
         Scanner sc = new Scanner(System.in);
-
-        /*
-         * array visin, dodajamo bloke tako da vedno preverjamo po vrsticah in polozimo blok ko pride do trka
-         * 
-         */
 
         int n = sc.nextInt();
         int l, x;
@@ -102,107 +96,43 @@ public class DN05_63230273{
             }
         };
 
-        int[] visineStolpcevPolja = new int[2001];
+        int[] visineStolpcevPolja = new int[2004];
 
         for(int i = 0; i < n; i++){
             l = sc.nextInt();
             x = sc.nextInt();
 
-            int sirinal = sirinaLika(liki[l]);
+            int sirinaLika = sirinaLika(liki[l]);
             int[] visineLika = visineLika(liki[l]);
-            int[] spodnjeVisineLika = spodnjeVisineLika(liki[l]);
+            int[] spodnjeVisineLika = spodnjeVisineLika(liki[l]);   // visine praznin od spodaj glede na najnižjo točko v liku
 
-            int maxVisinaIndex = 1000;
-            for(int j = x + 1000; j < x + 1000 + sirinal; j++){
-                if(visineStolpcevPolja[j] > visineStolpcevPolja[maxVisinaIndex]){
-                    maxVisinaIndex = j;
-                }
+            int maxVisinaIndex = najdiNajvisjiIndexNaObmočjuPadanjaLika(x, sirinaLika, visineStolpcevPolja);
+            int trenutnaMaxVisinaNaObmočjuPadanjaLika = visineStolpcevPolja[maxVisinaIndex];
+            
+            // izračun novih višin po padcu lika
+            for(int j = x + 1000; j < x + 1000 + sirinaLika; j++){
+                visineStolpcevPolja[j] = trenutnaMaxVisinaNaObmočjuPadanjaLika + (visineLika[j - (1000 + x)] - (visineStolpcevPolja[maxVisinaIndex] > 0 ? spodnjeVisineLika[maxVisinaIndex - (1000 + x)] : 0));
             }
-            
-            for(int j = x + 1000; j < x + 1000 + sirinal; j++){
-                System.out.printf("\n->%d, %d, %d\n", maxVisinaIndex, j, x);
-                int visina1 = (visineStolpcevPolja[maxVisinaIndex] + (visineLika[j - (1000 + x)] - spodnjeVisineLika[maxVisinaIndex - 1000 + x]));
-                int visina2 = -1;
-                if(spodnjeVisineLika[j - 1000 - x] == 0){
-                    visina2 = visineStolpcevPolja[j] + visineLika[j - 1000 - x];
-                }
-                visineStolpcevPolja[j] = Math.max(visina1, visina2);
-            }
-            
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-            
-            
         }
 
-            
-
-        //     if(maxVisinaIndex == 0){
-        //         for(int k = 0; k < liki[l][0].length; k++){
-        //             for(int j = 0; j < liki[l].length; j++){
-        //                 if(liki[l][j][k] == 1){
-        //                     visineStolpcevPolja[x + 999 + k] = liki[l].length - j + 1;
-        //                     break;
-        //                 }
-        //             }
-        //         }
-        //     }
-        //     // KAJ ČE PADE LIK 9 NA DESNO OD LIKA 14 ali LIK 13 PADE NA LEVI DEL LIKA 16
-            
-            
-
-
-        //     int odmikKoordinateTrka = Math.abs(maxVisinaIndex - x);
-        //     int pozicijay = 0;
-        //     for(int j = liki[l].length - 1; j >= 0; j--){
-        //         System.out.printf("\n[%d, %d, %d]\n", l, j, odmikKoordinateTrka);
-        //         if(liki[l][j][odmikKoordinateTrka] == 1){
-        //             pozicijay = j;  // koordinata y dela lika ki se dotakne prejšnjih
-        //             break;
-        //         }
-        //     }
-
-        //     for(int j = 0; j < liki[l].length; j++){
-        //         for(int k = 0; k < liki[l][j].length; k++){
-        //             if(liki[l][j][k] == 1){
-        //                 int visinskaRazlika = Math.abs(pozicijay - j);
-        //                 int novaVisina = visineStolpcevPolja[maxVisinaIndex] + visinskaRazlika + 1;//(liki[l].length - 1 - pozicijay) + visinskaRazlika;
-        //                 if(novaVisina > visineStolpcevPolja[x + 999 + k]){
-        //                     visineStolpcevPolja[x + 999 + k] = novaVisina;
-        //                 }
-        //             }
-        //         }
-        //     }
-
-        //     for(int j = pozicijay; j >= 0; j--){
-        //         for(int k = 0; k < liki[l][j].length; k++){
-        //             if(liki[l][j][k] == 1){
-        //                 visineStolpcevPolja[x + 999 + k]++;
-        //             }
-        //         }
-        //     }
-        // }
-
-        //izpis(visineStolpcevPolja);
+        izpis(visineStolpcevPolja);
 
         sc.close();
     }
 
+    private static int najdiNajvisjiIndexNaObmočjuPadanjaLika(int x, int sirinaLika, int[] visineStolpcevPolja){
+        int maxVisinaIndex = 0;
+        for(int j = x + 1000; j < x + 1000 + sirinaLika; j++){
+            if(visineStolpcevPolja[j] > visineStolpcevPolja[maxVisinaIndex]){
+                maxVisinaIndex = j;
+            }
+        }
+
+        return maxVisinaIndex;
+    }
 
     private static int[] visineLika(int[][] lik){
-        int[] visine = new int[lik[0].length]; // [3,]
+        int[] visine = new int[lik[0].length];
 
         for(int i = 0; i < lik[0].length; i++){
             for(int j = 0; j < lik.length; j++){
@@ -242,10 +172,10 @@ public class DN05_63230273{
         return 0;
     }
 
-    private static void izpis(int[] a){
-        for(int i = 0; i < a.length; i++){
-            if(a[i] > 0){
-                System.out.printf("%2d: %d\n", i - 999, a[i]);
+    private static void izpis(int[] visineStolpcevPolja){
+        for(int j = 0; j < visineStolpcevPolja.length; j++){
+            if(visineStolpcevPolja[j] > 0){
+                System.out.printf("%d: %d\n", (j - 1000), visineStolpcevPolja[j]);
             }
         }
     }
